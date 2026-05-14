c4354914f5967bcefbfdd5af1901d20c57872b5b
rulshrm <raflysahrul3114@gmail.com>
2026-05-14 15:04:53 +0700

    fix(api): remove transaction controller

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import authController from "../controllers/auth.controller";
-import transactionController from "../controllers/transaction.controller";
 import authMiddleware from "../middlewares/auth.middleware";
 
 const router = express.Router();
@@ -10,6 +9,5 @@ router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
 router.get("/auth/me", authMiddleware, authController.me);
 router.post("/auth/activation", authController.activation);
-
 
 export default router;
