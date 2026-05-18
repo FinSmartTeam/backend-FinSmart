3881e9170ebb075c0f3b7b02f5e39df5bb416efb
rulshrm <raflysahrul3114@gmail.com>
2026-05-18 08:30:10 +0700

    refactor: index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,17 @@ import cors from "cors";
 import router from "./routes/api";
 import docs from "./docs/route";
 
-async function init(){
+async function init() {
   try {
     const app = express();
     const PORT = 3000;
 
-    app.get("/",(req,res)=>{
+    app.get("/", (req, res) => {
       return res.status(200).json({
-        message:"FinSmart API is running",
-        data: null
-      })
-    })
+        message: "FinSmart API is running",
+        data: null,
+      });
+    });
 
     app.use(cors());
     app.use(bodyParser.json());
@@ -23,12 +23,11 @@ async function init(){
     app.use("/api", router);
     docs(app);
 
-app.listen(PORT, () => {
-  console.log(`Server run at port ${PORT}`);
-});
- 
+    app.listen(PORT, () => {
+      console.log(`Server run at port ${PORT}`);
+    });
   } catch (error) {
-    console.log("Error : ",error);
+    console.log("Error : ", error);
   }
 }
 
